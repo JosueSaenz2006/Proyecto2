71b2753793e679ceecab1a14eb5cb525f6c796e5
unknown <josuesaenz885@gmail>
2022-11-14 12:33:50 -0500

    clase de navegacion2

--- components/componente2/componente2.component.ts
+++ components/componente2/componente2.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-componente2',
@@ -8,12 +7,9 @@ import { Router } from '@angular/router';
 })
 export class Componente2Component implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-  navegacion(){
-    this.router.navigate(['web2'])
   }
 
 }
